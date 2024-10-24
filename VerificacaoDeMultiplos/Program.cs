aaaf40fa0c282e4b1cab63ee5d3a715b744d4b4b
Gabriela <gabriela.priaro@gmail.com>
2024-10-24 21:32:01 +0100

    Implementaçao para verificar se os numeros sao multiplos.

--- VerificacaoDeMultiplos/Program.cs
+++ VerificacaoDeMultiplos/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace VerificacaoDeMultiplos
+﻿//Problema "multiplos" (adaptado de URI 1044)
+//Fazer um programa para ler dois números inteiros, e dizer se um número é múltiplo do outro. Os
+//números podem ser digitados em qualquer ordem. 
+
+namespace VerificacaoDeMultiplos
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numero1, numero2;
+
+            Console.WriteLine("Digite dois numeros inteiros: ");
+            numero1 = int.Parse(Console.ReadLine());
+            numero2 = int.Parse(Console.ReadLine());
+            
+            if (numero1 == 0 || numero2 == 0)
+            {
+                return;
+            }
+
+            if (numero1 % numero2 == 0 || numero2 % numero1 == 0  )
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
         }
     }
 }
